d8c842a6366c31a262782837f74c548eb8f72459
sivasan2022 <grsivasankari79@gmail.com>
2024-12-03 19:23:07 +0530

    created login and register pages
    Signed-off-by: sivasan2022 <grsivasankari79@gmail.com>

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return
+  }
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
